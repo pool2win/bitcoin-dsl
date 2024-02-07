1c26e6d29bdb494cdeed954406f3f3d1580648fc
pool2win <pool2win@users.noreply.github.com>
2024-02-07 16:32:59 +0100

    Support signing transactions and use assertions
    Assertions are from test/unit

--- lib/ark.rb
+++ lib/ark.rb
@@ -7,16 +7,16 @@ pretty_print getblockchaininfo
 alice = key :new
 asp = key :new
 
-# Get P2WPKH address for Alice and mine blocks paying the address
+# Get P2WPKH address for Alice and mine blocks allowing coinbase spend
 address = alice.to_p2wpkh
-generatetoaddress num_blocks: 1, to: address
+generatetoaddress num_blocks: 101, to: address
 
-# Get and print the block to verify things are working
-blockhash = getblockhash hash: 1
+# Get the first block mined to alice
+blockhash = getblockhash height: 1
 block = getblock hash: blockhash, verbosity: 2
-pretty_print block
+# pretty_print block
 
-# Extract coinbase data to use as input in first spending transaction
+# Extract coinbase from first block for alice data to use as input in first spending transaction
 coinbase_amount = get_value block: block, tx_index: 0, vout_index: 0
 coinbase_txid = get_txid block: block, tx_index: 0
 coinbase_script_pubkey = get_script_pubkey block: block, tx_index: 0, vout_index: 0
@@ -37,11 +37,24 @@ alice_boarding_tx = transaction inputs: [
                                 outputs: [
                                   {
                                     address: asp.to_p2wpkh,
-                                    value: 49 * 1_000_000
+                                    value: 49.999 * 100_000_000
                                   }
                                 ],
                                 version: 2
 
 # Verify transaction is properly signed
 verification_result = alice_boarding_tx.verify_input_sig(0, coinbase_script_pubkey, amount: coinbase_amount)
-puts "Input signature verification: #{verification_result}"
+assert verification_result, 'Verifcation failed'
+
+accepted = testmempoolaccept rawtxs: [alice_boarding_tx.to_hex]
+assert accepted[0]['allowed'], "Alice boarding tx not accepted #{accepted.inspect}"
+
+send_result = sendrawtransaction tx: alice_boarding_tx.to_hex
+assert_equal send_result, alice_boarding_tx.txid, "Sending raw transaction failed. #{send_result}"
+
+generateblock to: alice.to_p2wpkh
+
+pretty_print getblockchaininfo
+
+query_result = getrawtransaction txid: alice_boarding_tx.txid
+assert query_result, 'Transaction not found in a confirmed block'
--- lib/run.rb
+++ lib/run.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: false
 
 require 'optparse'
+require 'test/unit'
 require_relative 'node'
 require_relative 'key'
+require_relative 'commands'
 
 ALICE_WIF = 'cRqVmDbyhw8kA9fWnAVJZDng3uTHfb71ELfpa8HBpTmkqLB2sA7f'
 ASP_WIF = 'cQQ4UPzo6css6cFMEMjmfz5mNXcDanYff6yLo8yCbJHj1ESW1Qnp'
@@ -21,14 +23,17 @@ puts "Running script from #{options[:script]}"
 
 # Runner interprets the bitcoin DSL using instance_eval
 class Runner
+  include  Test::Unit::Assertions
   # Use method missing to invoke bitcoin RPC commands
   def method_missing(method, *args, &_block)
+    super unless COMMANDS.include? method
     node method, *args
   end
 
-  def respond_to_missing?(_method, *)
-    true
+  def respond_to_missing?(method, *)
+    COMMANDS.include? method
   end
+
 end
 
 node :start
