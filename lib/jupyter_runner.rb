1e013fec5d8cfe20a32ae0c8b60a8428d9c5714f
pool2win <pool2win@users.noreply.github.com>
2024-03-31 21:24:29 +0200

    Add Kernel events to jupyter runner

--- lib/jupyter_runner.rb
+++ lib/jupyter_runner.rb
@@ -20,78 +20,20 @@
 # This is a monkey patch for the IRuby PlainBackend. The patch changes
 # the binding to the BitcoinDSL's Runner class.
 
-require 'singleton'
-require_relative 'node'
 require_relative 'runner'
 
-CustomBinding = Runner
+IRuby::Kernel.events.register(:before_initialize) do
+  IRuby.logger.warn 'Running before intialized'
+  runner = Runner.instance
+  IRuby::Kernel.custom_binding = runner
+end
 
 IRuby::Kernel.events.register(:initialized) do |_kernel|
-  `date +%s > /tmp/xxx`
   node :start
   IRuby.logger.info 'IRuby kernel has been initialized'
 end
 
 IRuby::Kernel.events.register(:before_shutdown) do |_kernel|
-  `date +%s > /tmp/xxx`
   node :stop
   IRuby.logger.info 'IRuby kernel is shutting down'
 end
-
-# class CustomIRBBinding
-#   def xxx_action
-#     puts 1000
-#     `date +%s > /tmp/xxx`
-#   end
-
-#   def before_stop
-#     IRuby.logger.info 'IRuby kernel shutting down'
-#     `date +%s > /tmp/xxx`
-#     IRuby.logger.info 'IRuby shutdown completed'
-#     # require_relative 'node'
-#     # node :stop
-#   end
-# end
-
-# IRuby::Kernel.events.register(:after_start) do |kernel|
-#   STDERR.puts 'IRuby kernel has been initialized'
-#   # require_relative './runner'
-#   # runner = Runner.new
-
-#   runner = Doom.new
-#   @workspace = IRB::WorkSpace.new(runner)
-#   @irb = IRB::Irb.new(@workspace)
-#   @eval_path = @irb.context.irb_path
-#   IRB.conf[:MAIN_CONTEXT] = @irb.context
-#   # node :start
-# end
-
-# module IRuby
-#   class PlainBackend
-#     def initialize
-#       IRuby.logger.info("Starting node")
-#       require 'irb'
-#       require 'irb/completion'
-
-#       # require_relative './runner'
-#       # runner = Runner.new
-#       runner = Doom.new
-#       IRB.setup(nil)
-#       @main = runner
-#       init_main_object(@main)
-#       @workspace = IRB::WorkSpace.new(@main)
-#       @irb = IRB::Irb.new(@workspace)
-#       @eval_path = @irb.context.irb_path
-#       IRB.conf[:MAIN_CONTEXT] = @irb.context
-#       node :start
-#     end
-
-#     # @private
-#     def shutdown_request(msg)
-#       IRuby.logger.info('Shuting down node')
-#       # node :stop
-#       @session.send(:reply, :shutdown_reply, msg[:content])
-#       @running = false
-#     end
-#   end
-# end
