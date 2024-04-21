9864fdefec5728ea8085409242c181b02bef4270
pool2win <pool2win@users.noreply.github.com>
2024-04-21 09:27:43 +0200

    Remove tweaked keys example

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -72,10 +72,6 @@ RSpec.describe Runner.instance do
     it_behaves_like 'script evaluation', './lib/contracts/reorg_chain.rb'
   end
 
-  describe 'tweaked keys' do
-    it_behaves_like 'script evaluation', './lib/contracts/using_tweaked_keys.rb'
-  end
-
   describe 'taproot transactions' do
     it_behaves_like 'script evaluation', './lib/contracts/taproot/keypath_spend.rb'
   end
