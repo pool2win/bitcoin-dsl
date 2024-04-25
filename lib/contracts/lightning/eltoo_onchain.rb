7ddc46843377c5342457c219e9b77f9e741aa2af
pool2win <pool2win@users.noreply.github.com>
2024-04-25 08:30:56 +0200

    Rename eltoo funding to setup

--- lib/contracts/lightning/eltoo_onchain.rb
+++ lib/contracts/lightning/eltoo_onchain.rb
@@ -35,32 +35,30 @@ OP_ENDIF
 
 transition :setup do
   extend_chain to: @alice, num_blocks: 101
-  @alice_funding_input_tx = spendable_coinbase_for @alice
-end
+  @alice_input_tx = spendable_coinbase_for @alice
 
-transition :alice_creates_funding do
-  @funding_tx = transaction inputs: [
-                              {
-                                tx: @alice_funding_input_tx,
-                                vout: 0,
-                                script_sig: 'sig:_skip'
-                              }
-                            ],
-                            outputs: [
-                              {
-                                script: @update_script,
-                                # descriptor: 'wsh(multi(2,@alice_settlement_key,@bob_settlement_key))',
-                                amount: 49.999.sats
-                              }
-                            ]
-  assert_not_mempool_accept @funding_tx
+  @setup_tx = transaction inputs: [
+                            {
+                              tx: @alice_input_tx,
+                              vout: 0,
+                              script_sig: 'sig:_skip'
+                            }
+                          ],
+                          outputs: [
+                            {
+                              script: @update_script,
+                              # descriptor: 'wsh(multi(2,@alice_settlement_key,@bob_settlement_key))',
+                              amount: 49.999.sats
+                            }
+                          ]
+  assert_not_mempool_accept @setup_tx
 end
 
 transition :bob_creates_settlement do
   # Bob signs a settlement transaction paying agreed amounts to Alice
   @settlement_tx = transaction inputs: [
                                  {
-                                   tx: @funding_tx,
+                                   tx: @setup_tx,
                                    vout: 0,
                                    script_sig: 'sig:multi(_empty,@bob_settlement_key) 0x01',
                                    csv: 10
@@ -74,11 +72,11 @@ transition :bob_creates_settlement do
                                ]
 end
 
-transition :alice_broadcasts_funding_tx do
+transition :alice_broadcasts_setup_tx do
   # Alice signs the funding transaction and broadcasts it
-  update_script_sig for_tx: @funding_tx, at_index: 0, with_script_sig: 'sig:wpkh(@alice)'
-  broadcast @funding_tx
-  confirm transaction: @funding_tx, to: @bob
+  update_script_sig for_tx: @setup_tx, at_index: 0, with_script_sig: 'sig:wpkh(@alice)'
+  broadcast @setup_tx
+  confirm transaction: @setup_tx, to: @bob
 end
 
 transition :alice_signs_settlement do
@@ -94,8 +92,7 @@ transition :alice_broadcasts_settlement do
 end
 
 run_transitions :setup,
-                :alice_creates_funding,
                 :bob_creates_settlement,
-                :alice_broadcasts_funding_tx,
+                :alice_broadcasts_setup_tx,
                 :alice_signs_settlement,
                 :alice_broadcasts_settlement
